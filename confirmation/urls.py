41aa8c9c02dc1bb468a7acd88a1f4db13201cb52
BradleyMurrell <b.murrell89@gmail.com>
2022-09-18 21:04:03 +0000

    Add complete order page

--- confirmation/urls.py
+++ confirmation/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.confirmation, name='confirmation')
+    path('confirmation', views.confirmation, name='confirmation'),
+    path('complete', views.complete, name='complete')
 ]
--- confirmation/views.py
+++ confirmation/views.py
@@ -4,3 +4,8 @@ def confirmation(request):
     """A view to return the confirmation page"""
 
     return render(request, 'confirmation/confirmation.html')
+
+def complete(request):
+    """A view to return the complete page"""
+
+    return render(request, 'confirmation/complete.html')
