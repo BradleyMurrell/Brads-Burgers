d01cf3239829bbb69df747876347de391b55738c
BradleyMurrell <b.murrell89@gmail.com>
2022-09-25 19:10:17 +0000

    Add order model and views

--- confirmation/models.py
+++ confirmation/models.py
@@ -3,5 +3,6 @@ from django.db import models
 
 class Order(models.Model):
     number = models.CharField(max_length=3)
+    name = models.CharField(max_length=100)
     total = models.DecimalField(max_digits=4, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True, null=True)
--- confirmation/views.py
+++ confirmation/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 import random
 import json
 
+
 def randomOrderNumber(length):
     sample = 'ABCDEFGH0123456789'
     numberO = ''.join((random.choice(sample) for i in range(length)))
@@ -15,15 +16,15 @@ def confirmation(request):
     request.session.set_expiry(0)
     if request.is_ajax():
         request.session['orders'] = request.POST.get('orders')
+        order = json.loads(request.session['orders'])
         request.session['total'] = request.POST.get('total')
-        order = json.loads(request.session['total'])
-        order = Order(number=randomOrderNumber(3), bill=float(request.session['total']))
-        order.save()
-
+        if request.user.is_authenticated:
+            order = Order(number=randomOrderNumber(3), name=order, total=float(request.session['total']))
+            order.save()
     return render(request, 'confirmation/confirmation.html')
 
 
 def success(request):
-    order = request.session['order']
+    order = request.session['orders']
     ctx = {'order': order}
     return render(request, 'confirmation/success.html', ctx)
